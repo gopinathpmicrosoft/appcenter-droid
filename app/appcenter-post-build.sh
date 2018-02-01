3c5fb2b257071fb56afc41edc2c339ffa97bd1a7
Gopinath P <gopip@microsoft.com>
2018-02-01 10:34:38 +0530

    Commit

--- app/appcenter-post-build.sh
+++ app/appcenter-post-build.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
 echo "Current branch is $APPCENTER_OUTPUT_DIRECTORY"
+curl -F "status=2" -F "ipa=@$APPCENTER_OUTPUT_DIRECTORY/MyApps.ipa" -H "X-HockeyAppToken: HOCKEYAPP_API_TOKEN" https://rink.hockeyapp.net/api/2/apps/HOCKEYAPP_APP_ID/app_versions/upload
