ef3d58efe658c2b623602b11b37c41e2c6647946
shamsha786 <shamsha786@gmail.com>
2018-02-02 03:49:16 -0800

    Test Post Build
    Testing hitting to azure function with file in req body.

--- app/appcenter-post-build.sh
+++ app/appcenter-post-build.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/env bash
-echo "Current branch is $APPCENTER_OUTPUT_DIRECTORY"
-curl -F "status=2" -F "ipa=@$APPCENTER_OUTPUT_DIRECTORY/MyApps.ipa" -H "X-HockeyAppToken: HOCKEYAPP_API_TOKEN" https://rink.hockeyapp.net/api/2/apps/HOCKEYAPP_APP_ID/app_versions/upload
+#echo "Current branch is $APPCENTER_OUTPUT_DIRECTORY"
+#curl -F "status=2" -F "ipa=@$APPCENTER_OUTPUT_DIRECTORY/MyApps.ipa" -H "X-HockeyAppToken: HOCKEYAPP_API_TOKEN" https://rink.hockeyapp.net/api/2/apps/HOCKEYAPP_APP_ID/app_versions/upload
+
+
+# Get the app from the current build
+current_Build_Package= "$APPCENTER_OUTPUT_DIRECTORY/*.ipa"
+
+# Call PRSS CodeSign For Andriod.
+curl -X POST --header 'Content-Type: multipart/form-data' --header 'Accept: application/json' 
+-F file=@"$APPCENTER_OUTPUT_DIRECTORY/*.ipa"  'https://andriodprsscodesign-dev.azurewebsites.net/api/HttpTriggerJS1'
+
+# Upload Signed bit if code sign passed.
