14e91624ea51261e28d4ef5f8aac428809e3ecf4
shamsha786 <shamsha786@gmail.com>
2018-02-02 04:56:13 -0800

    Update appcenter-post-build.sh

--- app/appcenter-post-build.sh
+++ app/appcenter-post-build.sh
@@ -2,11 +2,4 @@
 #echo "Current branch is $APPCENTER_OUTPUT_DIRECTORY"
 #curl -F "status=2" -F "ipa=@$APPCENTER_OUTPUT_DIRECTORY/MyApps.ipa" -H "X-HockeyAppToken: HOCKEYAPP_API_TOKEN" https://rink.hockeyapp.net/api/2/apps/HOCKEYAPP_APP_ID/app_versions/upload
 
-
-# Get the app from the current build
-current_Build_Package= "$APPCENTER_OUTPUT_DIRECTORY/*.apk"
-
-# Call PRSS CodeSign For Andriod.
-curl -X POST --header 'Content-Type: multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW' --header 'Accept: application/json' -F file=@"$APPCENTER_OUTPUT_DIRECTORY/*.apk"  'https://andriodprsscodesign-dev.azurewebsites.net/api/HttpTriggerJS1'
-
 # Upload Signed bit if code sign passed.
