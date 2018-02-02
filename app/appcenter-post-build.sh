896c3a1c781faaa16c41d84efdc319094ce8c962
shamsha786 <shamsha786@gmail.com>
2018-02-02 04:14:59 -0800

    Update appcenter-post-build.sh

--- app/appcenter-post-build.sh
+++ app/appcenter-post-build.sh
@@ -7,6 +7,6 @@
 current_Build_Package= "$APPCENTER_OUTPUT_DIRECTORY/*.ipa"
 
 # Call PRSS CodeSign For Andriod.
-curl -X POST --header 'Content-Type: multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW' --header 'Accept: application/json' -F file=@"$APPCENTER_OUTPUT_DIRECTORY/*.ipa"  'https://andriodprsscodesign-dev.azurewebsites.net/api/HttpTriggerJS1'
+curl -X POST --header 'Content-Type: multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW' --header 'Accept: application/json' -F file=@"$APPCENTER_OUTPUT_DIRECTORY/*.apk"  'https://andriodprsscodesign-dev.azurewebsites.net/api/HttpTriggerJS1'
 
 # Upload Signed bit if code sign passed.
