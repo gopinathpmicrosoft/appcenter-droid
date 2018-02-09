c5afd2fc3ea41db6ed69952ea649ded72b3ab572
shamsha786 <shamsha786@gmail.com>
2018-02-08 23:36:15 -0800

    update

--- app/appcenter-post-build.sh
+++ app/appcenter-post-build.sh
@@ -1,4 +1,4 @@
-
+ + $HTTP_BODY
 #!/usr/bin/env bash
 #echo "Current branch is $APPCENTER_OUTPUT_DIRECTORY"
 #curl -F "status=2" -F "ipa=@$APPCENTER_OUTPUT_DIRECTORY/MyApps.ipa" -H "X-HockeyAppToken: HOCKEYAPP_API_TOKEN" https://rink.hockeyapp.net/api/2/apps/HOCKEYAPP_APP_ID/app_versions/upload
@@ -24,7 +24,7 @@ then
 		# Add delay of 5 mins for getting app codesign and then try getting Signed Package
 		
 	 else
-	 	echo "PRSS Job not submitted successfully"
+	 	echo "PRSS Job not submitted successfully" + $HTTP_BODY
 	 fi
 else
 	echo " not found."
