1832c29e80c71d33b851aaf4bb4ba8db8ac351cb
shamsha786 <shamsha786@gmail.com>
2018-02-08 23:03:09 -0800

    Updating script

--- app/appcenter-post-build.sh
+++ app/appcenter-post-build.sh
@@ -7,12 +7,28 @@
 # Get the app from the current build
 if [ -f "$APPCENTER_OUTPUT_DIRECTORY/app-release.apk" ]
 then
-	echo " found."
+	echo " Release file found."
+	# Call PRSS CodeSign For Andriod.
+	HTTPRESPONSE=$(curl --write-out "HTTPSTATUS:%{http_code}" -X POST --header 'Content-Type: multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW' --header 'Accept: application/json' -F "apk=@$APPCENTER_OUTPUT_DIRECTORY/app-release.apk"  'https://andriodprsscodesign-dev.azurewebsites.net/api/HttpTriggerCSharp1?code=dSiBY8MLi48nS/UULIVmmnrmcjyDZYYRYfDtbxLNFa8Wry3pQ0rMrA==')
+	
+	# extract the body
+	HTTP_BODY=$(echo $HTTPRESPONSE | sed -e 's/HTTPSTATUS\:.*//g')
+
+	# extract the status
+	HTTP_STATUS=$(echo $HTTPRESPONSE | tr -d '\n' | sed -e 's/.*HTTPSTATUS://')
+
+	#Check for 200 response and get JobID
+	if "$HTTP_STATUS" -eq 200
+	then
+	 	echo "PRSS Job Submitted with Job ID " + $HTTP_BODY
+		# Add delay of 5 mins for getting app codesign and then try getting Signed Package
+		
+	 else
+	 
+	 fi
 else
 	echo " not found."
 fi
 
-# Call PRSS CodeSign For Andriod.
-curl -X POST --header 'Content-Type: multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW' --header 'Accept: application/json' -F "apk=@$APPCENTER_OUTPUT_DIRECTORY/app-release.apk"  'https://andriodprsscodesign-dev.azurewebsites.net/api/HttpTriggerJS1'
 
 # Upload Signed bit if code sign passed.
