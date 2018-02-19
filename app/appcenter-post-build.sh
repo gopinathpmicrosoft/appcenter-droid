6e69bb3d592491cd2f72d8ef6776719b2b3a1a32
shamsha786 <shamsha786@gmail.com>
2018-02-19 05:44:01 -0800

    Testing Mimem part for Hash

--- app/appcenter-post-build.sh
+++ app/appcenter-post-build.sh
@@ -9,14 +9,14 @@ then
 	echo " Release file found."
 	# Calculate hash and file size 4
 	#brew install sha2
-	sha256sum $APPCENTER_OUTPUT_DIRECTORY/app-release.apk
+	#sha256sum $APPCENTER_OUTPUT_DIRECTORY/app-release.apk
 	HASH=$(shasum -a 256 $APPCENTER_OUTPUT_DIRECTORY/app-release.apk | awk '{print $1}')
 	echo $HASH
 	FILESIZEINBYTES=$(wc -c $APPCENTER_OUTPUT_DIRECTORY/app-release.apk | awk '{print $1}')
 	echo $FILESIZEINBYTES
 	
 	# Call PRSS CodeSign For Andriod.adding esrpclient call
-	HTTP_RESPONSE_CSREQUEST=$(curl --write-out "HTTPSTATUS:%{http_code}" -X POST -F Hash='kAkAycGDEuvKMQF4vJbgJpDrIUS9PcwlrDkTlvM/pQw=' -F "apkfile=@$APPCENTER_OUTPUT_DIRECTORY/app-release.apk" -H 'Content-Type: multipart/form-data; charset=utf-8' 'https://andriodprsscodesign-dev.azurewebsites.net/api/HttpTriggerCSharp1?code=dSiBY8MLi48nS/UULIVmmnrmcjyDZYYRYfDtbxLNFa8Wry3pQ0rMrA==&Size=$FILESIZEINBYTES')
+	HTTP_RESPONSE_CSREQUEST=$(curl --write-out "HTTPSTATUS:%{http_code}" -X POST -F Hash=$HASH -F "apkfile=@$APPCENTER_OUTPUT_DIRECTORY/app-release.apk" -H 'Content-Type: multipart/form-data; charset=utf-8' 'https://andriodprsscodesign-dev.azurewebsites.net/api/HttpTriggerCSharp1?code=dSiBY8MLi48nS/UULIVmmnrmcjyDZYYRYfDtbxLNFa8Wry3pQ0rMrA==&Size=$FILESIZEINBYTES')
 	
 	# extract the body as json format
 	HTTP_BODY=$(echo $HTTP_RESPONSE_CSREQUEST | sed -e 's/HTTPSTATUS\:.*//g') 
