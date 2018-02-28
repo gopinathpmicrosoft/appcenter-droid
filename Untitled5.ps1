3b4181fba56c7cebbec542d717481e013c748b36
shamsha786 <shamsha786@gmail.com>
2018-02-28 17:37:08 +0530

    Add files via upload

---
+++ Untitled5.ps1
@@ -0,0 +1,17 @@
+﻿$azureAccountName ="949384c7-17be-4dc0-90fa-1ba17c7af451"
+$azurePassword = ConvertTo-SecureString "Password~1" -AsPlainText -Force
+$psCred = New-Object System.Management.Automation.PSCredential($azureAccountName, $azurePassword)
+
+Add-AzureRmAccount -Credential $psCred -TenantId '72f988bf-86f1-41af-91ab-2d7cd011db47' -ServicePrincipal
+
+Select-AzureRmSubscription -SubscriptionName 'Marketplace - UAT'
+
+$resourceGroup='Marketplace - UAT'
+$containerName='prssinput'
+
+$storageAccount = Get-AzureRmStorageAccount -ResourceGroupName 'VASC-PRSS-RG' -AccountName "anprsscodesignstorage"
+
+$ctx = $storageAccount.Context
+
+# download first blob
+Get-AzureStorageBlobContent -Container $containerName -Blob "app-release.apk"  -Destination $args[0] -Context $ctx
